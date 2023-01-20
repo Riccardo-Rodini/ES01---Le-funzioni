20e0f1b12e6e642faba72fdb16f2a0d01be8f0d0
Riccardo Rodini <113691534+Riccardo-Rodini@users.noreply.github.com>
2023-01-20 13:09:36 +0100

    Add files via upload

---
+++ somma_divisori.es.cpp
@@ -0,0 +1,16 @@
+/*****************************************************************************************
+* \mainpage    Scrivere una funzione chiamata "somma_divisori" che prende in input un    *
+*              numero intero e restituisce la somma dei suoi divisori.                   *
+*                                                                                        *
+*                                                                                        *             
+*  @brief    trovare la somma dei divisori                                               *
+*                                                                                        *
+*  @author    Rodini Riccardo                                                            *
+*                                                                                        *
+*  @date     11/01/2023                                                                  *
+*                                                                                        *
+*****************************************************************************************/
+
+#include<stdio.h>
+
+
